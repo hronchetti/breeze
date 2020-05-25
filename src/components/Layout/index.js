760d4a38394bd265d8df09203d7c7c7db61148c7
Harry Ronchetti <harry@ne6.digital>
2020-05-26 00:44:01 +0100

    removing console log for env

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -29,7 +29,6 @@ const Layout = ({ children, footer }) => (
       <>
         <Nav courses={data.allStrapiCourseTopics.edges} />
         {children}
-        {console.log(process.env.GATSBY_STRIPE)}
         {footer && (
           <Footer
             courses={data.allStrapiCourseTopics.edges}
