a3b5c4a55d2e762094a310f8c0cf5fe24e983142
Harry Ronchetti <harry@ne6.digital>
2020-06-06 14:36:54 +0100

    removing loader again

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,20 +1,11 @@
 import React from "react"
 import FacebookCustomerChat from "./src/components/FacebookCustomerChat"
-import { Loader } from "./src/components/Loader"
 
 export const wrapPageElement = (
   { element } // eslint-disable-line
 ) => (
   <React.Fragment>
-    <Loader />
     {element}
     <FacebookCustomerChat />
   </React.Fragment>
 )
-
-export const onClientEntry = () => {
-  window.onload = () => {
-    document.getElementById("loader") &&
-      document.getElementById("loader").classList.add("loaded")
-  }
-}
